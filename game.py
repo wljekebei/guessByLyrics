85e6a1cb56d303ae71182a0b170192e5ee3b7df1
wljekebei <rostik22062007@gmail.com>
2026-05-05 00:52:58 +0200

    getting lyrics from song

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 from ytmusicapi import YTMusic
+import random
 
 yt = YTMusic()
 
@@ -16,3 +17,13 @@ playlistURL = input('Enter playlist link: ')
 playlistID = playlistURL.split("list=", 1)[1].split("&", 1)[0]
 playlist = yt.get_playlist(playlistID)
 # print(playlist['title'], 'by ', playlist['author']['name'])
+
+song = random.choice(playlist['tracks'])
+# print(song['title'], 'by ', song['artists'][0]['name'])
+
+videoId = song['videoId']
+watchPlaylist = yt.get_watch_playlist(videoId=videoId)
+lyricsBrowseId = watchPlaylist.get('lyrics')
+
+songLyrics = yt.get_lyrics(lyricsBrowseId, False)
+print(songLyrics['lyrics'])
