5407abf03f4e4e69b9ebfc0500d2a0231d4d5de1
wljekebei <rostik22062007@gmail.com>
2026-05-05 03:07:07 +0200

    added possibility to choose the artist

--- game.py
+++ game.py
@@ -3,6 +3,8 @@ import random
 
 yt = YTMusic()
 
+## ПОФИКСИТЬ ОГРАНИЧЕНИЕ В 100 ТРЕКОВ С ПЛЕЙЛИСТА
+
 # search_results = yt.search('Oasis Wonderwall')
 # print(search_results[0]['title'], 'by', search_results[0]['artists'][0]['name'])
 
@@ -13,32 +15,50 @@ yt = YTMusic()
 #     if (playlist[i]):
 #         print(playlist[i]['title'], ' by ', playlist[i]['author'])
 
-playlistURL = input('Enter playlist link: ')
-playlistID = playlistURL.split("list=", 1)[1].split("&", 1)[0]
-playlist = yt.get_playlist(playlistID)
-# print(playlist['title'], 'by ', playlist['author']['name'])
+choice = int(input("Artist or playlist (1/2)? "))
+
+if (choice == 2):
+    playlistURL = input('Enter playlist link: ')
+    playlistID = playlistURL.split("list=", 1)[1].split("&", 1)[0]
+    playlist = yt.get_playlist(playlistID)
+    # print(playlist['title'], 'by ', playlist['author']['name'])
+else:
+    artistURL = input('Enter artist link: ')
+    channelID = artistURL.split("/channel/", 1)[1].split("?", 1)[0]
+    browseID = yt.get_artist(channelID)['songs']['browseId']
+    limit = int(input("Enter the amount of top songs you want to guess from: "))
+    playlist = yt.get_playlist(browseID, limit=limit)
+    # for track in playlist['tracks'][:limit]:
+    #     print(track['title'])
 
 while(True):
     print('\n')
-    song = random.choice(playlist['tracks'])
+    if (choice == 1):
+        song = random.choice(playlist['tracks'][:limit])
+    else:
+        song = random.choice(playlist['tracks'])
     # print(song['title'], 'by ', song['artists'][0]['name'])
 
     videoId = song['videoId']
     watchPlaylist = yt.get_watch_playlist(videoId=videoId)
     lyricsBrowseId = watchPlaylist.get('lyrics')
 
-    songLyrics = yt.get_lyrics(lyricsBrowseId, False)
-    # print(songLyrics['lyrics'])
+    try:
+        songLyrics = yt.get_lyrics(lyricsBrowseId, False)
+        # print(songLyrics['lyrics'])
+    except:
+        continue
+    
     words = songLyrics['lyrics'].split()
     wordCount = len(words)
     while (True):
-        firstWord = random.randint(0, (wordCount - 11))
+        firstWord = random.randint(0, (wordCount - 16))
         if(words[firstWord][0].isupper() == True):
             break
 
     guessed = False
-    points = 10
-    for i in range(firstWord, (firstWord + 10)):
+    points = 15
+    for i in range(firstWord, (firstWord + 15)):
         print('[', (points), ' pt] ', words[i])
         guessedName = input("Song name: ")
         if (guessedName.upper() == song['title'].upper()):
