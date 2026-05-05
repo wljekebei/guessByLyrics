24634f3f0eccea973a733e041ff37fc2445c0aeb
wljekebei <rostik22062007@gmail.com>
2026-05-05 02:05:13 +0200

    finished main logic

--- game.py
+++ game.py
@@ -18,12 +18,41 @@ playlistID = playlistURL.split("list=", 1)[1].split("&", 1)[0]
 playlist = yt.get_playlist(playlistID)
 # print(playlist['title'], 'by ', playlist['author']['name'])
 
-song = random.choice(playlist['tracks'])
-# print(song['title'], 'by ', song['artists'][0]['name'])
+while(True):
+    print('\n')
+    song = random.choice(playlist['tracks'])
+    # print(song['title'], 'by ', song['artists'][0]['name'])
 
-videoId = song['videoId']
-watchPlaylist = yt.get_watch_playlist(videoId=videoId)
-lyricsBrowseId = watchPlaylist.get('lyrics')
+    videoId = song['videoId']
+    watchPlaylist = yt.get_watch_playlist(videoId=videoId)
+    lyricsBrowseId = watchPlaylist.get('lyrics')
 
-songLyrics = yt.get_lyrics(lyricsBrowseId, False)
-print(songLyrics['lyrics'])
+    songLyrics = yt.get_lyrics(lyricsBrowseId, False)
+    # print(songLyrics['lyrics'])
+    words = songLyrics['lyrics'].split()
+    wordCount = len(words)
+    while (True):
+        firstWord = random.randint(0, (wordCount - 11))
+        if(words[firstWord][0].isupper() == True):
+            break
+
+    guessed = False
+    points = 10
+    for i in range(firstWord, (firstWord + 10)):
+        print('[', (points), ' pt] ', words[i])
+        guessedName = input("Song name: ")
+        if (guessedName.upper() == song['title'].upper()):
+            print('You guessed right! It is ', song['title'], ' by ', song['artists'][0]['name'], "\nYou got ", points, " points!")
+            guessed = True
+            break
+        elif (guessedName == ''):
+            points -= 1
+            continue
+        # elif (guessedName == 'stop'):
+        #     break
+        else:
+            points -= 1
+            print("Try again")
+
+    if (guessed == False):
+        print('The song was ', song['title'], ' by ', song['artists'][0]['name'])
